991a2c4ac087b2593f6e9addb9a676f77f63cdf5
Prince Christopher <pchristopher@fncinc.com>
2017-01-28 07:56:03 -0600

    Udpate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.change_code = 1;
 'use strict';
 
+var http = require('http');
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'tamilgreeter' );
 
@@ -8,6 +9,52 @@ var app = new alexa.app( 'tamilgreeter' );
 app.launch( function( request, response ) {
 	response.say( 'Welcome to your tamil greeter. Please tell your name.' ).reprompt( 'Can you please tell your name.').shouldEndSession( false );
 } );
+
+var getTodaysODBMP3 = function(req,res) {
+
+var options = {
+  host: 'odb.org',
+  port: 80,
+  path: '/'
+};
+
+var res = http.get(options, function(res) {
+var body = '';
+  console.log("Got response: " + res.statusCode);
+  //console.log(res.body);
+ res.on('data',function(data){
+   body += data;
+	});
+res.on('end', function() {
+//    console.log(body);
+  //  var audioSRC = process(body);
+    //var ssml = '<audio src="' + http://dzxuyknqkmi1e.cloudfront.net/odb/2017/01/odb-01-27-17.mp3+ '"/>';
+    //res.say(ssml); 
+    //var username = request.slot('USERNAME');
+    //res.say("Vanakkam "+ username + " "  + ssml);
+    return process(body);
+  });
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+	});
+
+res.end();
+
+}
+
+
+
+var process = function(body){
+	var x = /<audio\s+id="(.*?)"\s+src="(.*?)"(.*?)><\/audio>/gi;
+	var myArray = x.exec(body);
+	for( x of myArray){
+		console.log(x);
+	}
+	return x[2];
+}
+
+
+
 
 
 app.error = function( exception, request, response ) {
@@ -17,7 +64,7 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.intent('sayNumber',
+app.intent('WishInTamil',
   {
     "slots":{"USERNAME":"AMAZON.LITERAL"}
 	,"utterances":[ 
@@ -26,9 +73,17 @@ app.intent('sayNumber',
 		]
   },
   function(request,response) {
+    var audioSRC = getTodaysODBMP3(request,response);
+    //var audioSRC = process(body);
+    var ssml = '<audio src="http://dzxuyknqkmi1e.cloudfront.net/odb/2017/01/odb-01-27-17.mp3"/>';
+    //res.say(ssml); 
     var username = request.slot('USERNAME');
-    response.say("Vanakkam "+ username);
+    //p(request,response,username,ssml);
+    response.say("Vanakkam "+ username + " "  + ssml);
   }
 );
+
+var p = function(req,res,username,audiotag){
+}
 
 module.exports = app;
